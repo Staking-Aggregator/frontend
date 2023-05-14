139166bc704fac22943ebcb2a891962068635176
rishabhraghwendra@18 <rishabhraghwendra2002@gmail.com>
2023-05-14 14:01:12 +0530

    added providers & signer to valito state

--- src/utils/connectWallet.js
+++ src/utils/connectWallet.js
@@ -1,10 +1,14 @@
 import state from "../store";
+import { ethers } from 'ethers';
 
 const connectWallet = async () =>{
     if(window.ethereum){
       const walletAddress = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      state.provider = provider;
+      state.signer = signer;
       state.walletAddress = walletAddress[0];
-      console.log("click: ",walletAddress)
     }
     else{
       alert("Your browser don't have metamask installed. Please install it first.")
